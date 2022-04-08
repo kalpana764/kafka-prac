3a24248841bde54939b21f2b28fc54cf50c53397
kalpana dangi <kalpanakumari764@gmail.com>
2022-04-08 16:24:00 +0530

    Changes for rest controller

--- kafka/src/main/java/com/ss/org/controller/KafkaController.java
+++ kafka/src/main/java/com/ss/org/controller/KafkaController.java
@@ -5,7 +5,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("/test")
+@RestController
+@RequestMapping("/test")
 @Log4j2
 public class KafkaController {
 
